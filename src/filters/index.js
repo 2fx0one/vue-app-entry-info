75b264af81c455a532d7fac0e8e186a445a0e5da
2fx0one <2fx0one@gmail.com>
2018-12-10 16:06:34 +0800

    update

---
+++ src/filters/index.js
@@ -0,0 +1,60 @@
+// set function parseTime,formatTime to filter
+export { parseTime, formatTime } from '@/utils'
+
+function pluralize(time, label) {
+  if (time === 1) {
+    return time + label
+  }
+  return time + label + 's'
+}
+
+export function timeAgo(time) {
+  const between = Date.now() / 1000 - Number(time)
+  if (between < 3600) {
+    return pluralize(~~(between / 60), ' minute')
+  } else if (between < 86400) {
+    return pluralize(~~(between / 3600), ' hour')
+  } else {
+    return pluralize(~~(between / 86400), ' day')
+  }
+}
+
+// 格式化金额
+export function priceFormatNormal(s, n) {
+  n = n > 0 && n <= 20 ? n : 2
+  s = parseFloat((s + '').replace(/[^\d.-]/g, '')).toFixed(n) + ''
+  const l = s.split('.')[0].split('').reverse()
+  const r = s.split('.')[1]
+  let t = ''
+  for (let i = 0; i < l.length; i++) {
+    t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
+  }
+  return t.split('').reverse().join('') + '.' + r
+}
+// 格式化金额 人民币
+export function priceFormat(s, n, prefix) {
+  prefix = prefix || '￥' // 默认值￥
+  return prefix + priceFormatNormal(s, n)
+}
+
+/* 数字 格式化*/
+export function numberFormatter(num, digits) {
+  const si = [
+    { value: 1E18, symbol: 'E' },
+    { value: 1E15, symbol: 'P' },
+    { value: 1E12, symbol: 'T' },
+    { value: 1E9, symbol: 'G' },
+    { value: 1E6, symbol: 'M' },
+    { value: 1E3, symbol: 'k' }
+  ]
+  for (let i = 0; i < si.length; i++) {
+    if (num >= si[i].value) {
+      return (num / si[i].value + 0.1).toFixed(digits).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') + si[i].symbol
+    }
+  }
+  return num.toString()
+}
+
+export function toThousandslsFilter(num) {
+  return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
+}
